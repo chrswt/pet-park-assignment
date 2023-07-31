f1aebe5920b59b4ccf0e84a5e87e067db1473934
Christopher Tham <chriswt91@gmail.com>
2023-07-31 16:56:28 -0700

    Add borrow function

--- src/PetPark.sol
+++ src/PetPark.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 contract PetPark {
     address public owner;
 
-    mapping(AnimalType => uint256) public animalCount;
+    mapping(AnimalType => uint256) public animalCounts;
     mapping(address => User) private users;
 
     enum AnimalType {
@@ -29,7 +29,7 @@ contract PetPark {
 
     event Added(AnimalType indexed animalType, uint256 count);
     event Borrowed(AnimalType indexed animalType);
-    event Returned (AnimalType indexed animalType);
+    event Returned(AnimalType indexed animalType);
 
     constructor() {
         owner = msg.sender;
@@ -41,12 +41,38 @@ contract PetPark {
     }
 
     modifier validAnimal(AnimalType animalType) {
-        require(animalType != AnimalType.None, "Invalid animal");
+        require(animalType != AnimalType.None, "Invalid animal type");
         _;
     }
 
     function add(AnimalType animalType, uint256 count) external onlyOwner {
-        animalCount[animalType] += count;
+        animalCounts[animalType] += count;
         emit Added(animalType, count);
+    }
+
+    function borrow(
+        uint8 age,
+        Gender gender,
+        AnimalType animalType
+    ) external validAnimal(animalType) {
+        require(age > 0, "Age must be greater than zero");
+        require(animalCounts[animalType] > 0, "Selected animal not available");
+
+        User storage user = users[msg.sender];
+
+        // If the user does not exist, create new details for the current user
+        if (user.age == 0) {
+            user.age = age;
+            user.gender = gender;
+        } else {
+            require(
+                user.borrowedAnimal == AnimalType.None,
+                "Already adopted a pet"
+            );
+        }
+
+        animalCounts[animalType] -= 1;
+        user.borrowedAnimal = animalType;
+        emit Borrowed(animalType);
     }
 }
