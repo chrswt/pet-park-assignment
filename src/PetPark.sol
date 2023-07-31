35f44cd92c0f23daefb956bcce78a685f8a904a0
Christopher Tham <chriswt91@gmail.com>
2023-07-31 16:26:24 -0700

    Add AnimalType and Gender enumerated variables

--- src/PetPark.sol
+++ src/PetPark.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.13;
 
 contract PetPark {
-    
+    enum AnimalType {
+        none,
+        Fish,
+        Cat,
+        Dog,
+        Rabbit,
+        Parrot
+    }
+
+    enum Gender {
+        Male,
+        Female
+    }
 }
